4349bcdaf00cdef0a76adb22aebfeb03c6628135
AndrewProbert <andrewprobert15@gmail.com>
2023-07-02 12:16:19 -0400

    index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>GLD Track</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+        <style>
+            html, body, h1, h2, h3, h4, h5 {font-family: "Open Sans", sans-serif}
+        </style>
+    </head>
+
+    <body>
+        <a href="analysis.php">GLD Analysis</a>
+    </body>
