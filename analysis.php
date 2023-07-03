c30946b1f9e628bb8c46d1713e71ad418efe3617
AndrewProbert <andrewprobert15@gmail.com>
2023-07-02 20:55:07 -0400

    home page completed

---
+++ analysis.php
@@ -0,0 +1,238 @@
+<?php
+
+// Function to fetch data from Yahoo Finance API
+function fetchData($symbol, $startDate, $endDate)
+{
+    $url = "https://query1.finance.yahoo.com/v7/finance/download/$symbol?period1=$startDate&period2=$endDate&interval=1d&events=history";
+    $data = file_get_contents($url);
+    $rows = explode("\n", $data);
+    $headers = str_getcsv(array_shift($rows));
+    $result = [];
+
+    foreach ($rows as $row) {
+        $values = str_getcsv($row);
+        if (count($headers) == count($values)) {
+            $rowData = array_combine($headers, $values);
+            $rowData['Close'] = (float)$rowData['Close']; // Convert Close value to float
+            $result[] = $rowData;
+        }
+    }
+
+    return $result;
+}
+
+
+// Function to calculate RSI (Relative Strength Index)
+function calculateRSI($data, $period = 14)
+{
+    $rsi = [];
+    $count = count($data);
+    $diffs = array_column($data, 'Close', 'Date');
+    $lastClose = null;
+
+    foreach ($diffs as $date => $close) {
+        if ($lastClose !== null) {
+            $change = $close - $lastClose;
+            $diffs[$date] = $change;
+        }
+        $lastClose = $close;
+    }
+
+    $gains = [];
+    $losses = [];
+
+    foreach ($diffs as $diff) {
+        if ($diff > 0) {
+            $gains[] = $diff;
+            $losses[] = 0;
+        } elseif ($diff < 0) {
+            $gains[] = 0;
+            $losses[] = abs($diff);
+        } else {
+            $gains[] = 0;
+            $losses[] = 0;
+        }
+    }
+
+    $avgGain = array_sum(array_slice($gains, 0, $period)) / $period;
+    $avgLoss = array_sum(array_slice($losses, 0, $period)) / $period;
+
+    $rsi[] = 100 - (100 / (1 + ($avgGain / $avgLoss)));
+
+    for ($i = $period; $i < $count; $i++) {
+        $avgGain = (($avgGain * ($period - 1)) + ($gains[$i])) / $period;
+        $avgLoss = (($avgLoss * ($period - 1)) + ($losses[$i])) / $period;
+        $rsi[] = 100 - (100 / (1 + ($avgGain / $avgLoss)));
+    }
+
+    return $rsi;
+}
+
+// Function to calculate SMA (Simple Moving Average)
+function calculateSMA($data, $period)
+{
+    $sma = [];
+    $count = count($data);
+
+    for ($i = $period - 1; $i < $count; $i++) {
+        $sum = 0;
+        for ($j = $i - $period + 1; $j <= $i; $j++) {
+            if (isset($data[$j]['Close'])) {
+                $sum += $data[$j]['Close'];
+            }
+        }
+        $sma[] = $sum / $period;
+    }
+
+    return $sma;
+}
+
+
+// Function to calculate EMA (Exponential Moving Average)
+function calculateEMA($data, $period)
+{
+    $ema = [];
+    $count = count($data);
+    $multiplier = 2 / ($period + 1);
+    $sma = calculateSMA($data, $period);
+
+    for ($i = $period - 1; $i < $count; $i++) {
+        $sum = 0;
+        for ($j = $i - $period + 1; $j <= $i; $j++) {
+            if (isset($data[$j]['Close'])) {
+                $sum += $data[$j]['Close'];
+            }
+        }
+        $smaValue = $sum / $period;
+
+        if ($i == $period - 1) {
+            $ema[] = $smaValue;
+        } else {
+            $ema[] = (($data[$i]['Close'] - $ema[$i - $period]) * $multiplier) + $ema[$i - $period];
+        }
+    }
+
+    return $ema;
+}
+
+
+// Function to calculate MACD (Moving Average Convergence Divergence)
+function calculateMACD($data, $shortPeriod = 12, $longPeriod = 26, $signalPeriod = 9)
+{
+    $macd = [];
+    $emaShort = calculateEMA($data, $shortPeriod);
+    $emaLong = calculateEMA($data, $longPeriod);
+
+    $count = min(count($emaShort), count($emaLong)); // Use the minimum count of both arrays
+    for ($i = 0; $i < $count; $i++) {
+        if (isset($emaShort[$i]) && isset($emaLong[$i])) {
+            $macd[] = $emaShort[$i] - $emaLong[$i];
+        }
+    }
+
+    $signalCount = min(count($macd), $signalPeriod); // Use the minimum count of $macd array and $signalPeriod
+    $signal = calculateEMA(array_slice($macd, -$signalCount), $signalPeriod);
+
+    return [
+        'macd' => $macd,
+        'signal' => $signal,
+    ];
+}
+
+
+
+// Function to analyze when to buy call or put options
+function analyzeOptions($symbol)
+{
+    $endDate = strtotime('today');
+    $startDate = strtotime('-1 year', $endDate);
+    $data = fetchData($symbol, $startDate, $endDate);
+
+    $rsi = calculateRSI($data);
+    $sma = calculateSMA($data, 200);
+    $ema = calculateEMA($data, 200);
+    $macd = calculateMACD($data);
+
+    $lastRSI = end($rsi);
+    $lastSMA = end($sma);
+    $lastEMA = end($ema);
+    $lastMACD = end($macd['macd']);
+    $lastSignal = end($macd['signal']);
+
+    $confidenceScore = 0;
+
+    if ($lastRSI > 70 && $lastSMA > $lastEMA && $lastMACD > $lastSignal) {
+        $confidenceScore = 100; // High confidence
+    } elseif ($lastRSI > 60 && $lastSMA > $lastEMA && $lastMACD > $lastSignal) {
+        $confidenceScore = 80; // Medium confidence
+    } elseif ($lastRSI > 50 && $lastSMA > $lastEMA && $lastMACD > $lastSignal) {
+        $confidenceScore = 60; // Low confidence
+    }
+
+    if ($lastRSI < 30 && $lastSMA < $lastEMA && $lastMACD < $lastSignal) {
+        $confidenceScore = -100; // High confidence
+    } elseif ($lastRSI < 40 && $lastSMA < $lastEMA && $lastMACD < $lastSignal) {
+        $confidenceScore = -80; // Medium confidence
+    } elseif ($lastRSI < 50 && $lastSMA < $lastEMA && $lastMACD < $lastSignal) {
+        $confidenceScore = -60; // Low confidence
+    }
+
+    echo "Recommendation for $symbol: ";
+    if ($confidenceScore > 0) {
+        echo "Buy PUT options. Confidence Score: $confidenceScore";
+    } elseif ($confidenceScore < 0) {
+        echo "Buy CALL options. Confidence Score: " . abs($confidenceScore);
+    } else {
+        echo "No clear recommendation at the moment.";
+    }
+
+    //In this updated code, a confidence score of 100 indicates a high confidence recommendation, while a score of -100 represents a high confidence recommendation in the opposite direction. 
+    //The scores of 80 and 60 denote medium and low confidence, respectively. The scoring system allows for a more granular assessment of the confidence level, ranging from 0 to 100.
+}
+
+
+// Usage
+$symbol = 'GLD';
+//analyzeOptions($symbol);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Options Analysis</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+
+</head>
+<body>
+
+
+<header>
+    <nav>
+      <div class="logo">
+        <a href="index.php"> <img src="gloptionW.png" alt="Logo"></a>
+      </div>
+      
+
+
+
+      
+      <ul class="navbar">
+        <li><a href="analysis.php">Track</a></li>
+        <li><a href="#">Chat</a></li>
+        <li><a href="analysis.php">News</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+    <h1><br><br>Options Analysis</h1>
+
+    <?php
+    // Call the analyzeOptions function
+    analyzeOptions('GLD');
+    ?>
+
+    <!-- Add additional HTML content as needed -->
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -1,15 +1,103 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>GLD Track</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-     
-        <style>
-            html, body, h1, h2, h3, h4, h5 {font-family: "Open Sans", sans-serif}
-        </style>
-    </head>
+<head>
+  <title>Gloption</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
-    <body>
-        <a href="analysis.php">GLD Analysis</a>
-    </body>
+
+</head>
+<body>
+  <header>
+    <nav>
+      <div class="logo">
+        <a href="index.php"> <img src="gloptionW.png" alt="Logo"></a>
+      </div>
+      
+
+
+
+      
+      <ul class="navbar">
+        <li><a href="analysis.php">Track</a></li>
+        <li><a href="#">Chat</a></li>
+        <li><a href="analysis.php">News</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  
+  
+  <div class="content">
+
+
+  <h1><br><br>GLD SPDR Gold Shares Options Site</h1>
+  <p1>GLD is an ETF that tracks the price of gold. It is a popular investment for those who want to invest in gold without having to buy physical gold.</p1>
+  <p1>The purpose of this site is to help determine when investors should consider taking a position in GLD options.</p1>
+    <p1>It is not intended to be used as a sole source of information for making investment decisions.</p1>
+    <p1>It is recommended that you consult with a financial advisor before making any investment decisions.
+    <br> <br> 
+
+    </p1>
+
+
+  <div id="chart-container"></div>
+
+  <script type="text/javascript">
+    // Create a new TradingView widget instance
+    new TradingView.widget({
+      // Specify the container element
+      container_id: "chart-container",
+      // Define the symbol for GLD
+      symbol: "AMEX:GLD",
+      // Choose the desired interval (e.g., 'D' for daily, 'W' for weekly, 'M' for monthly)
+      interval: "D",
+      // Set the chart height
+      height: 500,
+      // Set the timezone (optional)
+      timezone: "Etc/UTC",
+      // Set the style of the chart
+      style: "1", // 1: "Light", 2: "Dark"
+      // Hide the top toolbar (optional)
+      hide_top_toolbar: true,
+      // Hide the bottom toolbar (optional)
+      hide_bottom_toolbar: true,
+      // Enable or disable the save/load chart functionality (optional)
+      save_image: false,
+      // Set the language (optional)
+      locale: "en",
+
+
+    });
+  </script>
+
+
+
+
+    <h1><br> How it Works</h1>
+    <p1></p1>
+    <p1>The website's decision to recommend buying a put or call option, or no position at all, is based on the analysis of several technical indicators. 
+        These indicators are used to assess the current market conditions and provide an indication of the potential direction of the stock's price.<br> <br> </p1>
+    <p1>The first indicator used is the Relative Strength Index (RSI), which measures the momentum of price movements. 
+        A high RSI value indicates that the stock may be overbought, while a low RSI value suggests it may be oversold.<br> <br> </p1>
+    <p1>The second indicator is the Simple Moving Average (SMA), which calculates the average price of the stock over a specified period. 
+        By comparing the current price to the SMA, the website can identify trends and potential support or resistance levels.<br> <br> </p1>
+    <p1>The third indicator is the Exponential Moving Average (EMA), which is similar to the SMA but places more weight on recent price data. 
+        The EMA helps to identify short-term trends and potential price reversals.
+        <br><br>  
+    </p1>
+
+    <p1>The fourth indicator is the Moving Average Convergence Divergence (MACD), which compares two EMAs of different periods. 
+        The MACD line indicates the relationship between the short-term and long-term price trends, while the signal line provides insights into potential trend reversals.<br> <br> </p1>
+    <p1>Based on the values of these indicators, the website assigns a confidence score to the recommendation. A positive score suggests a higher confidence in buying put options, indicating a potential downward movement in the stock's price. 
+        Conversely, a negative score suggests a higher confidence in buying call options, indicating a potential upward movement. 
+        If the confidence score is neutral (zero), no clear recommendation is provided.<br> <br> </p1>
+    <p1>It's important to note that these technical indicators provide insights into past price movements and trends and do not guarantee future performance. 
+        Additionally, the decision to buy put or call options should consider other factors such as fundamental analysis, market conditions, risk tolerance, and individual investment goals.<br> <br> </p1>
+    <p1></p1>
+
+  </div>
+</body>
+</html>
